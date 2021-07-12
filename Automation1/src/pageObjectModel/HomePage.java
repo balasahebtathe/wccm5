e7d68fcfddf77c33760cea09d4e245ac6888cb58
ANKUSH <ANKUSH@DESKTOP-FEK46KV>
2021-07-12 08:29:06 +0530

    new

--- Automation1/src/pageObjectModel/HomePage.java
+++ Automation1/src/pageObjectModel/HomePage.java
@@ -1,10 +1,34 @@
 package pageObjectModel;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		@FindBy(xpath="//div[text()='Setting']") private WebElement settings;
 
-	}
+		
+		
+		public HomePage(WebDriver driver) {
+			PageFactory.initElements(driver, this);
+		}
 
+
+
+		public WebElement getSettings()
+		{
+			return settings;
+		}
+		
+		
+		//action performing method
+		
+		public void clicksettings()
+		{
+			settings.click();
+		}
+		
+	
 }
--- Automation1/src/pageObjectModel/LoginPage.java
+++ Automation1/src/pageObjectModel/LoginPage.java
@@ -1,12 +1,85 @@
 package pageObjectModel;
 
-
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-	public static void main(String[] args) {
-		//declaration
+	//declaration
+		@FindBy(name="username") private WebElement usn;
+		@FindBy(name="pwd") private WebElement psw;
+		@FindBy(id="loginButton") private WebElement loginBtn;
+		@FindBy(id="forgotpass") private WebElement forgotpas;
+		
+		//initialization
+		public LoginPage(WebDriver driver) {
+			PageFactory.initElements(driver, this);
+		}
+		
+		
+		
+
+		//utilization
+		public WebElement getUsn() {
+			return usn;
+		}
+		public WebElement getPsw() {
+			return psw;
+		}
+		public WebElement getLogonBtn() {
+			return loginBtn;
+		}
+		
+		public WebElement getForgotpas() {
+			return forgotpas;
+		}
+
+		
+		//generic reusable method
+		public void validLogin(String username,String password)
+		{
+			usn.sendKeys(username);
+			psw.sendKeys(password);
+			loginBtn.click();
+			
+		}
+		
+		//action performing method
+		public void clickOnForgotpass()
+		{
+			forgotpas.click();
+		}
+		
+		public void invalidLoginMethod(String username,String password)
+		{
+			usn.sendKeys(username);
+			psw.sendKeys(password);
+			loginBtn.click();
+			
+		}
+		
+		
+		
+		public void setUsername(String username)
+		{
+			usn.sendKeys(username);
+		}
+		
+		public void setPassword(String password)
+		{
+			psw.sendKeys(password);
+		}
+		
+		public void clickOnLoginBtn()
+		{
+			loginBtn.click();
+		}
+		
+		public void clearUsername()
+		{
+			usn.clear();
+		}
+		
 		
 	}
-
-}
